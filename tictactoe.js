da18e3d053b597824cc3b56d148df2f0dfa9439f
Kyo young Lim <limkyouyou@gmail.com>
2024-02-25 20:11:48 -0500

    Clean duplicate codes in html and create basic player object in JS

--- tictactoe.js
+++ tictactoe.js
@@ -1,5 +1,9 @@
 
-// factory function for player. Takes  or 2 as parameter indicating which player they are
-function player() {
+// factory function for player. Takes o or x as parameter indicating which player they are
+function player(mark) {
 
+  return {mark};
 }
+
+var player_1 = player('o');
+var player_2 = player('x');
