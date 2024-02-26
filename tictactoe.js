1c5e646e562658d1fd61c921edeb38687fb61f6e
Kyo young Lim <limkyouyou@gmail.com>
2024-02-25 20:21:27 -0500

    Create primitive board object

--- tictactoe.js
+++ tictactoe.js
@@ -2,8 +2,20 @@
 // factory function for player. Takes o or x as parameter indicating which player they are
 function player(mark) {
 
-  return {mark};
+  const get_mark = () => mark;
+
+  return {get_mark};
+}
+
+function board() {
+
+  let board = [[0,0,0],[0,0,0],[0,0,0]];
+
+  const get_board = () => board;
+
+  return {get_board};
 }
 
 var player_1 = player('o');
 var player_2 = player('x');
+var new_board = board();
